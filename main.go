64675696b5ed293894a4967974609e5c7ea06623
Manuel de la Peña <mdelapenya@gmail.com>
2022-12-06 09:44:15 +0100

    chore: add flags and its usage

--- main.go
+++ main.go
@@ -4,16 +4,26 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/cli/go-gh"
 	"github.com/cli/go-gh/pkg/api"
 )
 
 var helpFlag bool
+var limitFlag int
+var queryFlag string
+var selectedPRNumbersFlag string
+var skipPRCheckFlag bool
+
 var ghClient api.RESTClient
 
 func init() {
 	flag.BoolVar(&helpFlag, "help", false, "Show help for multi-merge-prs")
+	flag.IntVar(&limitFlag, "limit", 50, "Sets the maximum number of PRs that will be combined. Defaults to 50")
+	flag.StringVar(&queryFlag, "query", "", `sets the query used to find combinable PRs. e.g. --query "author:app/dependabot to combine Dependabot PRs`)
+	flag.StringVar(&selectedPRNumbersFlag, "selected-pr-numbers", "", `COMMA,SEPARATED,LIST. If set, will only work on PRs with the selected numbers. e.g. --selected-pr-numbers 42,13,78`)
+	flag.BoolVar(&skipPRCheckFlag, "skip-pr-check", false, `if set, will combine matching PRs even if they are not passing checks. Defaults to false when not specified`)
 
 	client, err := gh.RESTClient(nil)
 	if err != nil {
@@ -26,11 +36,29 @@ func main() {
 	flag.Parse()
 
 	if helpFlag {
-		fmt.Println("hi world, this is the gh-multi-merge-prs extension!")
-		os.Exit(0)
+		usage()
 	}
 
 	whoami()
+}
+
+func usage() {
+	fmt.Println(`Usage: gh combine-prs --query "QUERY" [--limit 50] [--selected-pr-numbers 42,13,78] [--skip-pr-check] [--help]
+Arguments:
+	`)
+	maxLength := 0
+	flag.VisitAll(func(f *flag.Flag) {
+		if len(f.Name) > maxLength {
+			maxLength = len(f.Name)
+		}
+	})
+	flag.VisitAll(func(f *flag.Flag) {
+		currentLength := len(f.Name)
+		fmt.Fprintf(os.Stderr, "  --%s%s%s\n", f.Name, strings.Repeat(" ", maxLength-currentLength+3), f.Usage)
+	})
+
+	// exit execution after printing usage
+	os.Exit(0)
 }
 
 func whoami() {
