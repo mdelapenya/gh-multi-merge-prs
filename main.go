657e3decd575b02200654f390f82d91b7c5dfbf1
Manuel de la Peña <mdelapenya@gmail.com>
2022-12-06 09:29:33 +0100

    chore: parse a help flag

--- main.go
+++ main.go
@@ -1,13 +1,27 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
 
 	"github.com/cli/go-gh"
 )
 
+var helpFlag bool
+
+func init() {
+	flag.BoolVar(&helpFlag, "help", false, "Show help for multi-merge-prs")
+}
+
 func main() {
-	fmt.Println("hi world, this is the gh-multi-merge-prs extension!")
+	flag.Parse()
+
+	if helpFlag {
+		fmt.Println("hi world, this is the gh-multi-merge-prs extension!")
+		os.Exit(0)
+	}
+
 	client, err := gh.RESTClient(nil)
 	if err != nil {
 		fmt.Println(err)
